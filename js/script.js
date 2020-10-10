edc482e6b321e331ad854c96425a8d663b67d6d1
RenatoLomba <63821245+RenatoLomba@users.noreply.github.com>
2020-10-10 17:39:26 -0300

    contato

---
+++ js/script.js
@@ -0,0 +1,20 @@
+function validar() {
+
+    /*let lista_comentarios = ArrayBuffer('nome', 'sobre', 'sexo', 'email', 'cidade', 'estado', 'mensagem', 'newsletter')
+
+    lista
+ 
+    let nome = document.form_comentarios.nome.value
+    let sobre_nome = document.form_comentarios.snome.value
+    let sexo = document.form_comentarios.sexo.value
+    let email = document.form_comentarios.email.value
+    let cidade = document.form_comentarios.cidade.value
+    let estado = document.form_comentarios.estado.value
+    let mensagem = document.form_comentarios.mensagem.value
+    let newsletter = document.form_comentarios.newsletter.value
+
+    if(nome.length == 0) {
+        alert('Campo Nome em branco')
+    }*/
+
+}
