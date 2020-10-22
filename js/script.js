f7cfeec29000096c70ac54bef0bca1c5b1626431
RenatoLomba <63821245+RenatoLomba@users.noreply.github.com>
2020-10-22 13:23:44 -0300

    validacao

--- js/script.js
+++ js/script.js
@@ -33,3 +33,24 @@ function validarContato() {
     contato.avalie = document.form_contato.avalie.value
 
 }
+
+//Função para validar os campos invalidos e válidos no formulário de registro
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      // Fetch all the forms we want to apply custom Bootstrap validation styles to
+      var forms = document.getElementsByClassName('needs-validation');
+      // Loop over them and prevent submission
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+          validarRegistro() //IMPLEMENTAR VALIDAÇÃO DOS CAMPOS
+        }, false);
+      });
+    }, false);
+
+})();
